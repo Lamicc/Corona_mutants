2ee4ce3b6d8d6e8bb31ffbba5cdb9cb85f4ea3f3
leopardShark <lamicccc@gmail.com>
2020-04-22 16:26:57 +0200

    packing

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='virusnn',
+    version='0.1.0',
+    description='virus mutation prediction',
+    author='Koptagel, Samuelsson, Chen',
+    url='https://github.com/Lamicc/Corona_mutants',
+    packages=find_packages(include=['virusnn', 'virusnn.*']),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'tensorflow',
+        'h5py'
+    ]
+)
