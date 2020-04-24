2f460e379a519f2545e033c8ceb9d9c59a64cd79
leopardShark <lamicccc@gmail.com>
2020-04-24 15:44:39 +0200

    Create models.py

---
+++ virusnn/models.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# python 3 script
+
+import tensorflow as tf
+from tensorflow.keras.layers import *
+
+class CNN(tf.keras.Model):
+
+  def __init__(self):
+      super(CNN, self).__init__()
+      self.input = tf.keras.layers.InputLayer(input_shape=(d1,d2))
+      self.conv1d = tf.keras.layers.Conv1D(256, 3, activation='relu')
+      self.local1d = tf.keras.layers.LocallyConnected1D(128, 3, activation='relu')
+      self.pooling = tf.keras.layers.GlobalAveragePooling1D()
+      self.dropout = tf.keras.layers.Dropout(0.2)
+      self.dense = tf.keras.layers.Dense(4, activation='relu', use_bias=False)
+
+  def call(self, inputs):
+      x = self.input(inputs)
+      x = self.conv1d(x)
+      x = self.local1d(x)
+      x = self.conv1d(x)
+      x = self.local1d(x)
+      x = self.pooling(x)
+      x = self.dropout(x)
+
+      return self.dense(x)
