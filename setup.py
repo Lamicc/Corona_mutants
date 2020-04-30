f7f6eee0ff161cbfbd6d2674705ef1243017ce94
Johannes Asplund-Samuelsson <johannes.asplund.samuelsson@scilifelab.se>
2020-04-30 10:36:56 +0200

    Updated author

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='virusnn',
     version='0.1.0',
     description='virus mutation prediction',
-    author='Koptagel, Samuelsson, Chen',
+    author='Koptagel, Asplund-Samuelsson, Chen',
     url='https://github.com/Lamicc/Corona_mutants',
     packages=find_packages(include=['virusnn', 'virusnn.*']),
     install_requires=[
